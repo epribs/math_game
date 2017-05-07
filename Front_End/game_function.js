dcdf06411228754ac82f65b4591c818e263778a4
Travis Nash <travis.nash99@gmail.com>
2017-05-07 14:56:08 -0500

    game functions

--- Front_End/game_function.js
+++ Front_End/game_function.js
@@ -1,32 +1,27 @@
-var panel = $("#problemContainer");
-var countStartNumber = 60;
-
-var timer;
-
-var game = {
-
-  questions: questions,
-  currentQuestion: 0,
-  counter: countStartNumber,
-  correct: 0,
-  incorrect: 0,
-
-  countdown: function() {
-    this.counter--;
-    $("#timer").html(this.counter);
-    if (this.counter === 0) {
-      console.log("TIME UP");
-      this.timeUp();
-    }
-  },
-  loadQuestion: function() {
-
-    timer = setInterval(this.countdown.bind(this), 1000);
-
-    panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
-
-    for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
-      panel.append("<button class='answer-button' id='button' data-name='" + questions[this.currentQuestion].answers[i]
-      + "'>" + questions[this.currentQuestion].answers[i] + "</button>");
-    }
-  },
+// Game will have a fixed number of problems (20) within a fixed amount of time (1:00)
+// starts to count down timer, which is visible
+// Problems will be randomly generated, numbers will only be 1-10 and displayed in
+// 
+// WELCOME PAGE
+// User will log in, teachers and students use same log in form(ajax call)
+// Page will change to the game screen or report after clicking Submit button,
+// depending on user credentials/role
+// 
+// 
+// DURING GAME 
+// Once customer has logged in, a modal will appear with a Start button, 
+// User will press Start button(startButton)
+// At .onclick of start button, 1st problem and count down timer will be displayed
+// User will answer question in input form, press ENTER or click Submit button
+// When any question is answered, problemsRemainingBar will move down, until end of game
+// and will count up when answer is submitted.
+// If answer is correct,  correctBar will move up with problemsRemainingBar
+// else, correctBar doesn't move
+// 
+// 
+// END OF GAME 
+// Once game is over, results from rightAnswer, wrongAnswer vars
+// Once game is over, show resultsWindow, will cover progressPanel and 
+// If answer is correct, show results in the correctResults
+// If answer is incorrect, show results in the incorrectResults
+// If the session ends by time out, data is still saved. 
