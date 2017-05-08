3324a21bff2efc1b939fff7a68bbb3c12bbef021
megh0682 <shah.iyer@gmail.com>
2017-05-08 03:33:32 -0500

    db models refactor, passport authentication, login and logout routes for teacher and students

---
+++ models/teacher.js
@@ -0,0 +1,66 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var Teacher = sequelize.define("Teacher", 
+  {
+   name:{ 
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate:{
+        len:[1,100],
+        notEmpty:true
+      }
+    },
+    username:{ 
+    type: DataTypes.STRING,
+    allowNull: false,
+      validate:{
+        len:[4,50],
+        isAlphanumeric: true, 
+        notEmpty:true,
+        isUnique: function(value, next) {
+                  Teacher.find({
+                    where: {username: value},
+                    attributes: ['id']
+                   }).then(function(user) {
+                      if(user){
+                      console.log('username already in use!'); 
+                      return next('username already in use!');
+                    }
+                    console.log("username is available");
+                    next();
+                    }).catch(function(error){
+                      console.log(error);
+                      return next(error);
+                    });              
+        }
+      }
+    },
+    password:{ 
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate:{
+         notEmpty:true       
+      }
+    },
+    role:{ 
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate:{
+         notEmpty:true       
+      }
+    }
+   
+   },{
+
+  // disable the modification of tablenames; By default, sequelize will automatically
+  // transform all passed model names (first parameter of define) into plural.
+  // if you don't want that, set the following
+  freezeTableName: true,
+
+  }
+  );
+
+  return Teacher;
+};
+
