5819d6b343eca786d41e84668b432592be779065
epribs <ericpribramsky@gmail.com>
2017-05-11 20:42:56 -0500

    testing heroku db

--- server.js
+++ server.js
@@ -107,6 +107,9 @@ require("./routes/login-routes.js")(app);
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync({force:true}).then(function() {
   app.listen(PORT, function() {
+    db.Student.create({
+      name:"Eric"
+    }).then(function(x) {console.log(x)})
     console.log("App listening on PORT " + PORT);
   });
 });
