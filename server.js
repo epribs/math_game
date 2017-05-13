21fe923c1bc3ca777893f80686873763917446d3
epribs <ericpribramsky@gmail.com>
2017-05-12 23:39:21 -0500

    adding server

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(cookieParser());
+app.use(cookieParser());  
 
 // Static directory
 app.use(express.static(path.join(__dirname,"public")));
@@ -107,11 +107,6 @@ require("./routes/login-routes.js")(app);
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync({force:true}).then(function() {
   app.listen(PORT, function() {
-    db.User.create({
-      username: "eric_test",
-      password: "test123",
-      role: "teacher"
-    }).then(function(x) {console.log(x)})
     console.log("App listening on PORT " + PORT);
   });
 });
