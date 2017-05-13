03314d56fb6f71ed3e7e0e58906fc7888e5265b9
epribs <ericpribramsky@gmail.com>
2017-05-13 10:21:21 -0500

    fixing mysql

--- models/index.js
+++ models/index.js
@@ -14,7 +14,7 @@ var db        = {};
 //   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 // }
 
-var sequelize = new Sequelize(mysql://yw1b94efoh3ell4m:u7genq1yn2zazfn1@hngomrlb3vfq3jcr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/kipz2uz5u299o9ot,{
+var sequelize = new Sequelize("mysql://yw1b94efoh3ell4m:u7genq1yn2zazfn1@hngomrlb3vfq3jcr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/kipz2uz5u299o9ot",{
 		dialectOptions:{
 			ssl:true});
 }
